084c398729f6404b0d37a658bf76769ca113b147
AlexRenault <kubasovalex1969@icloud.com>
2023-06-13 20:08:50 +0300

    Задание 5 выполнено

--- Module14/05_years/main.py
+++ Module14/05_years/main.py
@@ -1,1 +1,20 @@
 # TODO здесь писать код
+
+def find(year):
+    work = year
+    digit = work % 10
+    count = 1
+    work //= 10
+    while work > 0:
+        if digit == work % 10:
+            count += 1
+        work //= 10
+    if count == 3:
+        print(year)
+
+
+start_year = int(input('Введите первый год: '))
+end_year = int(input('Введите последний год: '))
+
+for index in range(start_year, end_year + 1):
+    find(index)
