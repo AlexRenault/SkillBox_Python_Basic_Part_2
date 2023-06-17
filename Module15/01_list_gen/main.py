00c2b4f2b0d4848b23065c3eada8d3e60a44712a
AlexRenault <kubasovalex1969@icloud.com>
2023-06-17 09:35:22 +0300

    Задание 1 выполнено

--- Module15/01_list_gen/main.py
+++ Module15/01_list_gen/main.py
@@ -1,1 +1,10 @@
 # TODO здесь писать код
+number = int(input('Введите целое число, больше 0: '))
+
+num_list = []
+
+for index in range(1, number + 1, 2):
+    num_list.append(index)
+
+print('Список из нечетных чисел от 1 до ', number, ':', end=' ')
+print(num_list)
