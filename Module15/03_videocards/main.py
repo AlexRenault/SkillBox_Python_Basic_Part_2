748af76d36cd43a5bcee9946398a2399f47f6137
AlexRenault <kubasovalex1969@icloud.com>
2023-06-17 17:31:09 +0300

    Задание 3 выполнено

--- Module15/03_videocards/main.py
+++ Module15/03_videocards/main.py
@@ -1,3 +1,27 @@
 # TODO здесь писать код
+number_videocard = int(input('Введите количество видеокарт: '))
+videocard_list = []
+new_list = []
+
+for index in range(number_videocard):
+    print(index + 1, 'Видеокарта: ', end='')
+    videocard = int(input())
+    videocard_list.append(videocard)
+
+max_video = max(videocard_list)
+
+#max_video = videocard_list[0]
+#for videocard in videocard_list:
+#    if max_video < videocard:
+#        max_video = videocard
+
+for videocard in videocard_list:
+    if max_video != videocard:
+        new_list.append(videocard)
+
+videocard_list.clear
+videocard_list = new_list
+
+print(videocard_list)
 
 
