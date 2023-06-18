da799ade2f5cce719b8633cc44f008bd7d4ae425
AlexRenault <kubasovalex1969@icloud.com>
2023-06-18 15:36:19 +0300

    Задание 7 выполнено

--- Module15/07_word_analysis_2/main.py
+++ Module15/07_word_analysis_2/main.py
@@ -1,1 +1,23 @@
 # TODO здесь писать код
+word = input('Введите слово: ')
+word_list = []
+
+word_list.extend(word)
+
+# С использование нарезки
+reverse_word = word_list[:: -1]
+
+# С использованием функции reversed
+#reverse_word = list(reversed(word_list))
+
+# С использование цикла for
+#for index in range(len(word_list) - 1, -1, -1):
+#    reverse_word.append(word_list[index])
+
+if word_list == reverse_word:
+    print('Слово является полиндромом.')
+else:
+    print('Слово не является полиндромом.')
+
+print(word_list)
+print(reverse_word)
