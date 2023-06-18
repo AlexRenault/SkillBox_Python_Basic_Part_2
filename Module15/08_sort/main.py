6edf995bfea124b9d43d4ec6b63126a135978ce9
AlexRenault <kubasovalex1969@icloud.com>
2023-06-18 16:15:48 +0300

    Задание 8 выполнено. Реализованы 2 варианта решения.

--- Module15/08_sort/main.py
+++ Module15/08_sort/main.py
@@ -1,1 +1,20 @@
 # TODO здесь писать код
+num_list = [1, 4, -3, 0, 10]
+
+print('Изначальный список: ', num_list)
+
+# Вариант 1
+#for index in range(len(num_list) - 1):
+#    for sort_index in range(len(num_list) - 1):
+#        if num_list[sort_index] > num_list[index]:
+#            num_list[sort_index], num_list[index] = num_list[index], num_list[sort_index]
+
+# Вариант 2
+for index in range(len(num_list) - 1):
+    min_num = num_list[index]
+    for number in range(index, len(num_list) - 1):
+        if min_num > num_list[number]:
+            min_num, num_list[number] = num_list[number], min_num
+    num_list[index] = min_num
+
+print('\nОтсортированный список: ', num_list)
