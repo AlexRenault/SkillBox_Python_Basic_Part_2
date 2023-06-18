a4b2d707e4e45985976ef385a8ccd75372089790
AlexRenault <kubasovalex1969@icloud.com>
2023-06-18 12:11:32 +0300

    Задание 6 выполнено

--- Module15/06_running_nums/main.py
+++ Module15/06_running_nums/main.py
@@ -1,1 +1,18 @@
 # TODO здесь писать код
+elements_list = [1, 2, 3, 4, 5, 6]
+
+shift = int(input('Сдвиг: '))
+while shift > len(elements_list):
+    print('В списке всего ', len(elements_list), 'элементов.')
+    shift = int(input('Задайте величину сдвига, меньше длины списка: '))
+
+print('Изначальный список: ', elements_list)
+
+for i_shift in range(shift, 0, -1):
+    shift_element = elements_list[len(elements_list) - 1]
+    for index in range(len(elements_list) - 1, - 1, -1):
+        elements_list[index] = elements_list[index - 1]
+    elements_list[0] = shift_element
+
+print('Сдвинутый список: ', elements_list)
+
