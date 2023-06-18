d4c8fa0ec713e20da448ea00343759fe718a5a17
AlexRenault <kubasovalex1969@icloud.com>
2023-06-18 11:26:05 +0300

    Задание 5 выполнено

--- Module15/05_containers/main.py
+++ Module15/05_containers/main.py
@@ -1,1 +1,29 @@
 # TODO здесь писать код
+num_containers = int(input('Введите количество контейнеров: '))
+containers = []
+
+for i_cont in range(num_containers):
+    print('Введите вес', i_cont + 1, ' контейнера: ', end='')
+    weigth = int(input())
+    while (weigth > 200) or (weigth <= 0):
+        print('Ошибка! Вес контейнера должен быть больше 0 и меньше 200 кг. Повторите ввод.')
+        print('Введите вес', i_cont + 1, ' контейнера: ', end='')
+        weigth = int(input())
+    containers.append(weigth)
+
+new_cont = int(input('\nВведите вес нового контейнера: '))
+while (new_cont > 200) or (new_cont <= 0):
+    print('Ошибка! Вес контейнера должен быть больше 0 и меньше 200 кг. Повторите ввод.')
+    new_cont = int(input('\nВведите вес нового контейнера: '))
+
+count = len((containers))
+index = -1
+while (abs(index) <= len(containers)) and (new_cont > containers[index]):
+    count -= 1
+    index -= 1
+
+if abs(index) <= len(containers):
+    print('\nНомер, который получит новый контейнер: ', count + 1)
+else:
+    print('\nКонтейнер получит номер 1, т.к. он самый тяжелый.')
+
