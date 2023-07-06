ab0563b7ff6ab2ef5eca267c0db467704309354d
AlexRenault <kubasovalex1969@icloud.com>
2023-07-06 20:00:38 +0300

    Задание 5 выполнено.

--- Module16/05_songs/main.py
+++ Module16/05_songs/main.py
@@ -11,3 +11,21 @@ violator_songs = [
 ]
 
 # TODO здесь писать код
+num_songs = int(input('Сколько песен выбрать? '))
+sound_time = 0
+play_list = []
+
+for i_song in range(num_songs):
+    print(f'Название {i_song + 1}-й песни: ', end='')
+    name_song = input()
+
+    for song in violator_songs:
+        if name_song == song[0]:
+            play_list.append(song)
+            sound_time += song[1]
+            break
+    else:
+        print('Такой песни нет в списке.')
+
+print('Ваш плейлист: ', play_list)
+print('Общее время звучания песен: ', round(sound_time, 2))
