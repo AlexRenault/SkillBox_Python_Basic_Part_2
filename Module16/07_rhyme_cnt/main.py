92aa8ead35a924b6a97e61ff2338f23023a62324
AlexRenault <kubasovalex1969@icloud.com>
2023-07-08 16:06:15 +0300

    Задание 7 выполнено.

--- Module16/07_rhyme_cnt/main.py
+++ Module16/07_rhyme_cnt/main.py
@@ -1,1 +1,17 @@
 # TODO здесь писать код
+number = int(input('Количество человек: '))
+humans = list(range(1, number + 1))
+
+remove = int(input('Какое число в считалке? '))
+
+remove_human = 0
+
+while len(humans) != 1:
+    print('\nТекущий круг людей: ', humans)
+    index = remove_human % len(humans)
+    remove_human = (index + remove - 1) % len(humans)
+    print('начало отсчета с номера ', humans[index])
+    print('Выбывает человек под номером ', humans[remove_human])
+    humans.remove(humans[remove_human])
+
+print('Остался человек под номером ', humans[0])
