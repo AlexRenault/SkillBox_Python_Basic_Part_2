23da2db5396687815f0bbb3bd9f1e4998b18c260
AlexRenault <kubasovalex1969@icloud.com>
2023-07-11 19:04:36 +0300

    Задание 3 выполнено

--- Module17/03_random_competition/main.py
+++ Module17/03_random_competition/main.py
@@ -1,1 +1,15 @@
 # TODO здесь писать код
+import random
+
+number = int(input('Введите количество участников в командах: '))
+
+first_team = [round(random.triangular(5, 10), 2) for _ in range(number)]
+second_team = [round(random.triangular(5, 10), 2) for _ in range(number)]
+
+print('Первая команда: ', first_team)
+print('Вторая команда: ', second_team)
+
+winner = [first_team[index] if first_team[index] > second_team[index] else second_team[index]
+          for index in range(number)]
+
+print('Победители тура: ', winner)
