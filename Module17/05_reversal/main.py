94ca8861820baa5c2f71544ca2cac69c8ad8ad75
AlexRenault <kubasovalex1969@icloud.com>
2023-07-11 20:41:11 +0300

    Задание 5 выполнено

--- Module17/05_reversal/main.py
+++ Module17/05_reversal/main.py
@@ -1,1 +1,8 @@
 # TODO здесь писать код
+user_string = input('Введите строку, в которой буква h встречается как минимум два раза: ')
+
+new_string = user_string[user_string.find('h') + 1:user_string.rfind('h')]
+
+print('Развернутая последовательность между первым и последним h: ', new_string[::-1])
+
+
