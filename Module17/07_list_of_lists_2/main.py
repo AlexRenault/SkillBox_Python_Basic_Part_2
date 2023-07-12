1d2bccd31248c6c9bc6802388ca2772e8e03cc7e
AlexRenault <kubasovalex1969@icloud.com>
2023-07-12 19:07:08 +0300

    Задание 7 - удалены лишние пустые строки

--- Module17/07_list_of_lists_2/main.py
+++ Module17/07_list_of_lists_2/main.py
@@ -4,5 +4,3 @@ nice_list = [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
 # TODO здесь писать код
 new_list = [nice_list[idx_1][idx_2][idx_3] for idx_1 in range(2) for idx_2 in range(3) for idx_3 in range(3)]
 print('Ответ: ', new_list)
-
-
