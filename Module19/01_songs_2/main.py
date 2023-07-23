079afd070ed7afd50e723939ef3d7e553a968bf9
AlexRenault <kubasovalex1969@icloud.com>
2023-07-23 17:17:53 +0300

    Задача 1 решена

--- Module19/01_songs_2/main.py
+++ Module19/01_songs_2/main.py
@@ -11,3 +11,15 @@ violator_songs = {
 }
 
 # TODO здесь писать код
+count_song = int(input('Сколько песен выбрать? '))
+songs_time = 0
+
+for i_song in range(count_song):
+    print('Название {} песни: '.format(i_song + 1), end='')
+    song = input()
+    if song in violator_songs.keys():
+        songs_time += violator_songs[song]
+    else:
+        print('Извините. Песни {} в нашем списке нет.'.format(song))
+
+print('Общее время звучания песен: ', round(songs_time, 2))
