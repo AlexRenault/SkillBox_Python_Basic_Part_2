5294a73b15c1bc1057962fa06834538d614605f8
AlexRenault <kubasovalex1969@icloud.com>
2023-07-24 19:05:29 +0300

    Задание 5 выполнено

--- Module19/05_synonym_dict/main.py
+++ Module19/05_synonym_dict/main.py
@@ -1,1 +1,28 @@
 # TODO здесь писать код
+pair_words = int(input('Введите количество пар слов: '))
+synonyms = dict()
+
+for index in range(pair_words):
+    text = input(f'{index + 1} пара: ').lower()
+    pair = text.split()
+    for i_pair in pair:
+        if not i_pair.isalpha():
+            pair.remove(i_pair)
+    synonyms[pair[0]] = pair[1]
+
+print(synonyms)
+
+print('\nПоиск синонимов. Для завершения введите "стоп.')
+user_word, synonim = '', ''
+
+while user_word != 'стоп':
+    user_word = input('Введите слово: ').lower()
+    if user_word in synonyms.keys():
+        synonim = 'Синоним: ' + synonyms[user_word].title()
+    elif user_word in synonyms.values():
+        key_print = [key for key, value in synonyms.items() if user_word == value]
+        synonim = 'Синоним: ' + key_print[0].title()
+    elif user_word != 'стоп':
+        synonim = 'Такого слова в словаре нет.'
+
+    print(synonim)
