ff49540fc48c34bbea3e829a5b7bbdf1bbaed944
AlexRenault <kubasovalex1969@icloud.com>
2023-08-01 21:18:22 +0300

    Задание 5 выполнено

--- Module20/05_sort_function/main.py
+++ Module20/05_sort_function/main.py
@@ -1,1 +1,11 @@
 # TODO здесь писать код
+def sort_tpl(user_tpl):
+    if sum(list(user_tpl[:])) % 1 == 0:
+        user_tpl = tuple(sorted(list(tuple(user_tpl))))
+    return user_tpl
+
+
+# user_tuple = (6, 3, -1, 8, 4, 2, 10, -5)
+user_tuple = (6, 3, -1, 8.1, 4, 2, 10, -5)
+
+print(sort_tpl(user_tuple))
