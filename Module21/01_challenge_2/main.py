d32a48bbda6a9e30f4ed358fad7a9a00d60f614d
AlexRenault <kubasovalex1969@icloud.com>
2023-08-06 10:30:09 +0300

    Задание 1 выполнено

--- Module21/01_challenge_2/main.py
+++ Module21/01_challenge_2/main.py
@@ -1,1 +1,13 @@
 # TODO здесь писать код
+def out_numbers(num):
+    if num == 0:
+        return 0
+    else:
+        out_numbers(num - 1)
+        print(num)
+        num -= 1
+
+
+number = int(input('Введите число: '))
+
+out_numbers(number)
