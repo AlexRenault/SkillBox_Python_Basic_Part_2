708589d8aa1c8d67b412190050c6f87c5469deb3
AlexRenault <kubasovalex1969@icloud.com>
2023-08-11 21:19:20 +0300

    Задание 4 выполнено

--- Module21/04_advanced_sum/main.py
+++ Module21/04_advanced_sum/main.py
@@ -1,1 +1,24 @@
 # TODO здесь писать код
+def summ_f(*args, lst=[]):
+    data = []
+
+    for item in args:
+        if isinstance(item, list):
+            data = item
+        else:
+            data = list(args)
+            break
+
+    for item in data:
+        if isinstance(item, list):
+            summ_f(item)
+        else:
+            lst.append(item)
+
+    return sum(lst)
+
+
+# [[1, 2, [3]], [1], 3]
+# 1, 2, 3, 4, 5
+
+print('Ответ в консоли: ', summ_f([[[[1, 2, [3]], [1]], 3]]))
