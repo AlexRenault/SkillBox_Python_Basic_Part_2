da0184cdc76a48c2249e5166cb5b998b84ebd688
AlexRenault <kubasovalex1969@icloud.com>
2023-08-12 17:56:14 +0300

    Задание 4 исправлено в соответствии с замечаниями

--- Module21/04_advanced_sum/main.py
+++ Module21/04_advanced_sum/main.py
@@ -1,24 +1,16 @@
 # TODO здесь писать код
-def summ_f(*args, lst=[]):
-    data = []
+def summ_f(*args):
+    data_sum = 0
 
     for item in args:
-        if isinstance(item, list):
-            data = item
-        else:
-            data = list(args)
-            break
-
-    for item in data:
-        if isinstance(item, list):
-            summ_f(item)
-        else:
-            lst.append(item)
-
-    return sum(lst)
+        if isinstance(item, (int, float)):
+            data_sum += item
+        elif isinstance(item, (list, tuple)):
+            data_sum += summ_f(*item)
+    return data_sum
 
 
 # [[1, 2, [3]], [1], 3]
 # 1, 2, 3, 4, 5
 
-print('Ответ в консоли: ', summ_f([[[[1, 2, [3]], [1]], 3]]))
+print('Ответ в консоли: ', summ_f(1, 2, 3, 4, 5))
