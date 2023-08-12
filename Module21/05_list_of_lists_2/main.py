78c39734d8a418b5fc190e495d4bec7ac1e162ef
AlexRenault <kubasovalex1969@icloud.com>
2023-08-12 18:01:51 +0300

    Задание 5 исправлено в соответствии с замечаниями

--- Module21/05_list_of_lists_2/main.py
+++ Module21/05_list_of_lists_2/main.py
@@ -4,11 +4,11 @@ nice_list = [1, 2, [3, 4], [[5, 6, 7], [8, 9, 10]],
 
 # TODO здесь писать код
 
-def ext_list(user_lst, lst=[]):
-
+def ext_list(user_lst):
+    lst = []
     for item in user_lst:
         if isinstance(item, list):
-            ext_list(item)
+            lst += ext_list(item)
         else:
             lst.append(item)
     return lst
