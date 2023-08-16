1593cfb7434e050c8e63e885801498bbae8a416e
AlexRenault <kubasovalex1969@icloud.com>
2023-08-16 22:04:00 +0300

    Задание 3 выполнено.

--- Module22/03_files_and_folders/main.py
+++ Module22/03_files_and_folders/main.py
@@ -1,1 +1,29 @@
 # TODO здесь писать код
+import os
+
+dirs_count = 0
+files_count = 0
+size_folder = 0
+
+# use_path = '/Users/domasnij//SkillBox/Основы Python. Часть 2/Python_Basic/Python_Basic'
+use_path = input('Введите путь: ')
+os.chdir(use_path)
+print()
+print(os.getcwd())
+elem = []
+
+for dirs_path, dirs_name, files in os.walk(use_path):
+    for folder in dirs_name:
+        dirs_count += 1
+    for file in files:
+        files_count += 1
+        size_folder += os.path.getsize(os.path.join(dirs_path, file))
+        elem.append(os.path.join(dirs_path, folder, file))
+
+print()
+print('Количество подкаталогов: ', dirs_count)
+print('Количество файлов: ', files_count)
+print('Размер каталога в килобайтах: ', size_folder / 1024)
+
+for i_elem in elem:
+    print(i_elem)
