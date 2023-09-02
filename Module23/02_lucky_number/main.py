223a50b3e1c9d2a3855f3b599b06fbcdb8bfe275
AlexRenault <kubasovalex1969@icloud.com>
2023-09-02 19:18:22 +0300

    Задание 2 выполнено.

--- Module23/02_lucky_number/main.py
+++ Module23/02_lucky_number/main.py
@@ -1,1 +1,27 @@
 # TODO здесь писать код
+import random
+
+except_list = [ValueError, ZeroDivisionError, FileNotFoundError, TabError, TypeError, ArithmeticError]
+
+summ = 0
+lucky_number = 777
+
+try:
+    with open('out_file.txt', 'w', encoding='utf-8') as file_out:
+        while summ < lucky_number:
+            number = int(input('Введите число: '))
+            if random.randint(1, 13) == 13:
+                name_except = except_list[random.randint(0, 5)]
+                raise name_except
+            summ += number
+            file_out.write(''.join([str(number), '\n']))
+
+    print('Вы успешно выполнили условие для выхода из порочного цикла!')
+
+except ValueError as err:
+    print('Обязательно нужно было ввести число.', err, type(err))
+except name_except:
+    print(f'Произошла ошибка. Выполнение программы прервано', name_except, type(name_except))
+finally:
+    if not file_out.closed:
+        file_out.close()
