576b25f2a3872461147d1aba9a4d44a54f9ba158
AlexRenault <kubasovalex1969@icloud.com>
2023-09-22 11:52:05 +0300

    Задание 3 - выполнено.

--- Module25/03_my_dict/main.py
+++ Module25/03_my_dict/main.py
@@ -1,1 +1,13 @@
 # TODO здесь писать код
+class MyDict(dict):
+    def get(self, key, default=0):
+        return super().get(key, default)
+
+
+control = MyDict({i: i * 2 for i in range(10)})
+print(control)
+print()
+for idx in range(len(control)):
+    print('Если ключ есть: ', control.get(idx),
+          '\t Если ключа нет: ', control.get(idx + 10))
+
