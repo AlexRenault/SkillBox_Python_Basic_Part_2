013b226f584149366856e025900de646637430a6
AlexRenault <kubasovalex1969@icloud.com>
2023-11-03 11:01:22 +0300

    Задание 1 - выполнено

--- Module27/01_how_are_you/main.py
+++ Module27/01_how_are_you/main.py
@@ -1,1 +1,19 @@
 # TODO здесь писать код
+from typing import Callable, Any
+
+
+def how_are_you(func) -> Any:
+    """ Функция декоратор."""
+    def wrapped_func(*args, **kwargs) -> Any:
+        input('Как твои дела? ')
+        print('А у меня не очень.')
+        return func
+    return wrapped_func()
+
+
+@how_are_you
+def test():
+    print('Это была шутка.')
+
+
+test()
