e379eab49da0234c0576f962b8f36381736fb5cc
AlexRenault <kubasovalex1969@icloud.com>
2023-11-19 12:52:14 +0300

    Задание 3 - выполнено

--- Module28/03_date/main.py
+++ Module28/03_date/main.py
@@ -1,1 +1,39 @@
 # TODO здесь писать код
+import datetime
+
+
+class Date:
+    def __init__(self, string):
+        self.date = string
+
+    def __str__(self):
+        return "Число: {0}\t Месяц: {1}\t Год: {2}".format(
+            self.date[0], self.date[1], self.date[2])
+
+    @classmethod
+    def is_date_valid(cls, string):
+        try:
+            dat = datetime.datetime.strptime(string, "%d-%m-%Y")
+            if dat:
+                return True
+        except ValueError:
+            return False
+
+    @classmethod
+    def from_string(cls, string):
+        if cls.is_date_valid(string):
+            dat = string.split("-")
+            cls.date = Date(dat)
+            return cls.date
+
+
+date = Date.from_string('10-12-2077')
+print(date)
+print('\nПроверка на корректность:')
+print('10-12-2077 - ', Date.is_date_valid('10-12-2077'))
+print('10-13-2077 - ', Date.is_date_valid('10-13-2077'))
+
+
+
+
+
