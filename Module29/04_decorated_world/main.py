3ab3b3e0d90feac02c88aac2bfd0ff3e208262c6
aleksandr_kubasov <ductile.condo-0d@icloud.com>
2023-11-27 13:38:02 +0300

    Задание 4 - выполнено.

--- Module29/04_decorated_world/main.py
+++ Module29/04_decorated_world/main.py
@@ -1,1 +1,27 @@
 # TODO здесь писать код
+from typing import Callable
+
+
+def decorator_with_args_for_any_decorator(decorator: Callable) -> Callable:
+    """ Декоратор декораторов. Можно передавать любые аргументы."""
+    def wrap_decor(*args, **kwargs):
+        def decor(func: Callable) -> Callable:
+            print("Переданные арги и кварги в декоратор: ", *args, **kwargs)
+            return func
+        return decor
+    return wrap_decor
+
+
+@decorator_with_args_for_any_decorator
+def decorated_decorator(func: Callable) -> Callable:
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+    return wrapper
+
+
+@decorated_decorator(100, 'рублей', 200, 'друзей')
+def decorated_function(text: str, num: int) -> None:
+    print("Привет", text, num)
+
+
+decorated_function("Юзер", 101)
