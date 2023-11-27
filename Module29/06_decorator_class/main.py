518e549a5bd16e090a8ce895bd35fef68e36205b
aleksandr_kubasov <ductile.condo-0d@icloud.com>
2023-11-27 15:57:58 +0300

    Задание 6 - выполнено.

--- Module29/06_decorator_class/main.py
+++ Module29/06_decorator_class/main.py
@@ -1,4 +1,22 @@
 # TODO здесь писать код
+import time
+from typing import Callable
+import functools
+
+
+class LoggerDecorator:
+    def __init__(self, func: Callable) -> None:
+        functools.update_wrapper(self, func)
+        self.func = func
+        self.start = time.time()
+
+    def __call__(self, *args, **kwargs):
+        print('Вызов функции {}'.format(self.func.__name__))
+        print('Аргументы - ', *args, **kwargs)
+        print('Результат - ', self.func(*args, **kwargs))
+        time_exec = time.time() - self.start
+        print('время выполнения - ', time_exec)
+        return self.func(*args, **kwargs)
 
 
 @LoggerDecorator
@@ -7,9 +25,9 @@ def complex_algorithm(arg1, arg2):
     result = 0
     for i in range(arg1):
         for j in range(arg2):
-            with open('test.txt', 'w', encoding='utf8') as file:
-                file.write(str(i + j))
-                result += i + j
+            result += i + j
+    with open('test.txt', 'w', encoding='utf8') as file:
+        file.write(str(i + j))
     return result
 
 
