2fdf8585a51618d34e58ae481dff292bdeeb0d7e
aleksandr_kubasov <ductile.condo-0d@icloud.com>
2023-12-06 12:58:50 +0300

    Задание 3 - выполнено.

--- Module30/03_palindrome_the_return/main.py
+++ Module30/03_palindrome_the_return/main.py
@@ -1,1 +1,15 @@
 # TODO здесь писать код
+import collections
+
+
+def can_be_poly(string: str) -> bool:
+    count = collections.Counter(string)
+    odd = len(list(filter(lambda x: x % 2 != 0, list(count.values()))))
+    if (len(string) % 2 == 0 and odd == 0) or (len(string) % 2 != 0 and odd == 1):
+        return True
+    else:
+        return False
+
+
+print('abcba - ', can_be_poly('abcba'))
+print('abbbc - ', can_be_poly('abbbc'))
