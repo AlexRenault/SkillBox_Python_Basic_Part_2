4cf7e6c40d633567199e92757d7afadfd39ab08b
aleksandr_kubasov <ductile.condo-0d@icloud.com>
2023-12-07 17:31:54 +0300

    Задание 2 - выполнено.

--- Module31/02_registration_marks/main.py
+++ Module31/02_registration_marks/main.py
@@ -1,1 +1,13 @@
 # TODO здесь писать код
+import re
+
+number = 'А578ВЕ777 ОР233787 К901МН666 СТ46599 СНИ2929П777 666АМР666'
+
+person_pattern = (r'\b[А, В, Е, К, М, Н, О, Р, С, Т, У, Х]\d{3}'
+                  r'[А, В, Е, К, М, Н, О, Р, С, Т, У, Х]{2}\d{2,3}')
+print('Список номеров частных автомобилей: ')
+print(re.findall(person_pattern, number))
+
+print('\nСписок номеров такси: ')
+taxi_pattern = r'\b[А, В, Е, К, М, Н, О, Р, С, Т, У, Х]{2}\d{5,6}'
+print(re.findall(taxi_pattern, number))
