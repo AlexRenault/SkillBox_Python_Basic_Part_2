c5e18c8494201c6518d886ea55466395ad3cd5f3
aleksandr_kubasov <ductile.condo-0d@icloud.com>
2023-12-08 15:27:18 +0300

    Задание 3 - получение информации с сайта вынесено в отдельную функцию.

--- Module31/03_may_the_force_be_with_you/main.py
+++ Module31/03_may_the_force_be_with_you/main.py
@@ -1,9 +1,11 @@
 # TODO здесь писать код
-import requests, re
+import requests
 import json
+from typing import Any
 
 
-def find_key(struct, key):
+def find_key(struct, key) -> Any:
+    """ поиск заданного ключа key в структуре struct"""
     if key in struct:
         return struct[key]
     else:
@@ -17,24 +19,31 @@ def find_key(struct, key):
     return res
 
 
+def get_website(address) -> Any:
+    """получение информации с сайта address"""
+    result = requests.get(address)
+    if result == 200:
+        return json.loads(result.text)
+    return None
+
+
 address = 'https://swapi.dev/api/starships/'
-starships = requests.get(address)
-dict_starships = json.loads(starships.text)
+dict_starships = get_website(address)
 
 ship_list = find_key(dict_starships, 'results')
 
-mill_falcon = list((filter(lambda x: x['name'] == 'Millennium Falcon', ship_list)))
-mill_falcon = mill_falcon[0]
+star_ship = list((filter(lambda x: x['name'] == 'Millennium Falcon', ship_list)))
+star_ship = star_ship[0]
 
 ship_dict = dict()
-ship_dict['StarShip'] = {'Name': mill_falcon['name'],
-                         'Max speed': mill_falcon['max_atmosphering_speed'],
-                         'Class': mill_falcon['starship_class']}
+ship_dict['StarShip'] = {'Name': star_ship['name'],
+                         'Max speed': star_ship['max_atmosphering_speed'],
+                         'Class': star_ship['starship_class']}
 
 pilots_dict = dict()
-for item in mill_falcon['pilots']:
-    pilot = json.loads(requests.get(item).text)
-    home_world = json.loads(requests.get(pilot['homeworld']).text)
+for item in star_ship['pilots']:
+    pilot = get_website(item)
+    home_world = get_website(pilot['homeworld'])
     pilots_dict[pilot['name']] = {'Height': pilot['height'], 'Mass': pilot['mass'],
                                   'Home World': home_world['name'],
                                   'Website address': pilot['homeworld']}
