74fd69935274525a3b296e5d4954da3ce8dd1ab0
aleksandr_kubasov <ductile.condo-0d@icloud.com>
2023-12-08 17:33:33 +0300

    Задание 5 - выполнено.

--- Module31/05_web_scrapping/main.py
+++ Module31/05_web_scrapping/main.py
@@ -1,6 +1,13 @@
 # TODO здесь писать код
+import requests, re
 
 # В данном случае запрос request.get заменен на загрзку сайта из файла html
 with open('examples.html', 'r') as f:
     text = f.read()
 # По итогу вы так же получаете код сайта в виде одной строки
+
+pattern = r'(?<=>).+(?=</h3>)'
+print(re.findall(pattern, text))
+
+result = requests.get('https://ru.wikipedia.org/wiki/Яндекс_Маркет')
+print(re.findall(pattern, result.text))
