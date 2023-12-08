3c326d9c6cb543f368637a066bb8fc13af2271c0
aleksandr_kubasov <ductile.condo-0d@icloud.com>
2023-12-08 18:39:34 +0300

    Задание 6 - выполнено.

--- Module31/06_JSON_comparison/main.py
+++ Module31/06_JSON_comparison/main.py
@@ -1,1 +1,54 @@
 # TODO здесь писать код
+import json
+from typing import Any
+
+
+def read_from_file(file_name) -> dict:
+    with open(file_name, 'r') as file:
+        res = json.load(file)
+    return dict(res)
+
+
+def find_key(struct: Any, key: str) -> Any:
+    """ поиск заданного ключа key в структуре struct"""
+    if key in struct:
+        return struct[key]
+    else:
+        for sub in struct.values():
+            if isinstance(sub, dict):
+                res = find_key(sub, key)
+                if res:
+                    break
+        else:
+            res = None
+    return res
+
+
+def comparison(dict_1: dict, dict_2: dict) -> dict:
+    global diff_list
+
+    changes = dict()
+    for key in diff_list:
+        value_old = find_key(dict_1, key)
+        value_new = find_key(dict_2, key)
+        # print('Key:', key)
+        # print(value_old)
+        # print(value_new, end='\n\n')
+        if value_old != value_new:
+            changes[key] = value_new
+    return changes
+
+
+def save_to_file(value: dict) -> None:
+    with open('result.json', 'w') as file:
+        json.dump(value, file, indent=4)
+
+
+diff_list = ["services", "staff", "datetime"]
+
+res_old = read_from_file('json_old.json')
+res_new = read_from_file('json_new.json')
+
+result = comparison(res_old, res_new)
+print(result)
+save_to_file(result)
