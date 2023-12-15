b22031c31ca3ca5efae3a0fa234320dc5d061775
aleksandr_kubasov <ductile.condo-0d@icloud.com>
2023-12-15 13:42:55 +0300

    Задание 6 - добавлен поиск ключей в списках, кортежах, множествах.

--- Module31/06_JSON_comparison/main.py
+++ Module31/06_JSON_comparison/main.py
@@ -17,6 +17,16 @@ def find_key(struct: Any, key: str) -> Any:
         for sub in struct.values():
             if isinstance(sub, dict):
                 res = find_key(sub, key)
+                if isinstance(res, (list, tuple, set)):
+                    for item in res:
+                        if isinstance(item, dict):
+                            result = find_key(item, key)
+                            if result:
+                                break
+                    else:
+                        result = None
+                    if result is not None:
+                        res = result
                 if res:
                     break
         else:
@@ -31,9 +41,6 @@ def comparison(dict_1: dict, dict_2: dict) -> dict:
     for key in diff_list:
         value_old = find_key(dict_1, key)
         value_new = find_key(dict_2, key)
-        # print('Key:', key)
-        # print(value_old)
-        # print(value_new, end='\n\n')
         if value_old != value_new:
             changes[key] = value_new
     return changes
@@ -44,10 +51,12 @@ def save_to_file(value: dict) -> None:
         json.dump(value, file, indent=4)
 
 
-diff_list = ["services", "staff", "datetime"]
+diff_list = ["services", "staff", "datetime", "attendance"]
 
 res_old = read_from_file('json_old.json')
+print(res_old)
 res_new = read_from_file('json_new.json')
+print(res_new)
 
 result = comparison(res_old, res_new)
 print(result)
